6e25966199818c318658ebd8805dd71ccffe1a31
dnikolina5 <nikolina.dojcinovic@fpmoz.sum.ba>
2026-04-30 13:27:55 +0200

    Add files via upload

---
+++ zadacamatrice1.py
@@ -0,0 +1,28 @@
+#Napisati program koji generira kvadratnu matricu dimenzija 7x7.
+#Elementi su nasumični brojevi od 1 do 9.
+#Zatim napisati program koji računa zbroj elemenata na rubovima matrice.
+
+import random
+r = 7
+s = 7
+matrica = []
+
+for i in range(r):
+    red = []
+    for j in range(s):
+        unos = random.randint(1,9)
+        red.append(unos)
+    matrica.append(red)
+
+print("-----Matrica-----")
+for red in matrica:
+    print(red)
+
+zbroj = 0
+for i in range(r):
+    for j in range(s):
+        if i == 0 or i == r-1 or j == 0 or j == s-1:
+            zbroj += matrica[i][j]
+
+print("Zbroj elemenata na rubovima je:", zbroj)
+
